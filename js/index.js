ce7dfa8efc7e04d0a469d9a5f6a0b5ce1a1c3f93
Adeyemi Esther <63353240+Xtarachiever@users.noreply.github.com>
2020-11-06 13:48:04 +0100

    *updated-styles

--- js/index.js
+++ js/index.js
@@ -4,17 +4,20 @@ const navSlide=()=>{
     const menu=document.querySelector('#navigations a')
     const fa_times=document.querySelector('.fa-times')
     const fa_bars=document.querySelector('.fa-bars')
+    const context=document.querySelector('.all-context')
     navBar.addEventListener('click',()=>{
         lists.classList.toggle('listToggle');
         if(menu.innerHTML==='Close'){
             menu.innerHTML='Menu';
             fa_bars.style.display='block'
             fa_times.style.display='none'
+            // context.classList.remove('blur')
         }
         else{
             menu.innerHTML='Close';
             fa_times.style.display='block'
             fa_bars.style.display='none'
+            // context.classList.add('blur')
         }
     },false)
 }
