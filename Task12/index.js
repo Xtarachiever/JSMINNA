27a31e4bc7a5727bcdfbecb471e373bded2b133d
Adeyemi Esther <estheradeyemi796@gmail.com>
2020-11-29 10:19:25 +0100

    Updated

--- Task12/index.js
+++ Task12/index.js
@@ -29,7 +29,7 @@ const allFunctions=()=>{
             const textChange=document.getElementById('textChange')
             const body=document.querySelector('body')
             console.log(hour)
-            if(hour.value<='12'){
+            if(hour.value<'16'){
                 body.classList.remove('nightView')
                 body.classList.add('dayView')
                 weatherP.innerText='Good Morning'
