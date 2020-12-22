4f67741c69950b06d30f7d3ba47e5d772c230620
Adeyemi Esther <63353240+Xtarachiever@users.noreply.github.com>
2020-12-23 00:02:28 +0100

    updated

--- Project/JS/suggestions.js
+++ Project/JS/suggestions.js
@@ -1,13 +1,37 @@
-const suggestions=document.querySelector('.suggestions');
-const fetchSuggestions=async () =>{
-    const response=await fetch('https://jsminnastore.herokuapp.com/suggested/electronics',{
-        method:'GET',
-        headers:{
-            "Content-Type":"application/json; charset=UTF-8",
-            "Authorization":'Bearer ' + localStorage.getItem('token')
+const suggestions=document.querySelector('.suggestion');
+const allFunctions=()=>{
+    const generateSuggested=({itemCategory,itemDescription,itemName,reason})=>{
+        const suggestion=document.createElement('section');
+        suggestion.innerHTML=`
+        <div class="suggests">
+            <h3 class="itemName">${itemName}</h3>
+            <p class="itemCategory">${itemCategory}</p>
+            <p class="itemDescription">${itemDescription}</p>
+            <p class="reason">${reason}</p>
+        </div>
+        `
+        suggestions.appendChild(suggestion);
+    }
+    const generateSuggestions=(idea)=>{
+        for(const suggest of idea){
+            generateSuggested(suggest);
         }
-    });
-    const data=response.json();
-    console.log(data)
+    }
+    const fetchSuggestions=async () =>{
+        const response=await fetch('https://jsminnastore.herokuapp.com/suggested/electronics',{
+            method:'GET',
+            headers:{
+                "Content-Type":"application/json; charset=UTF-8",
+                "Authorization":'Bearer ' + localStorage.getItem('token')
+            }
+        });
+        response.json()
+        .then(dats=>{
+            console.log(dats.payload.result)
+            generateSuggestions(dats.payload.result)
+        })
+        // console.log(dats)
+    }
+    fetchSuggestions();
 }
-fetchSuggestions();
+allFunctions();
